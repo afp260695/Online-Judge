e36ac55c745c723209fe869d7c466b8dafcf3c70
Ahmad Fajar Prasetiyo <ahmad.fajar260695@gmail.com>
2018-08-22 10:23:59 +0700

    Initial Commit

---
+++ Online Judge/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Online Judge
+//
+//  Created by Ahmad Fajar Prasetiyo on 22/08/18.
+//  Copyright © 2018 ITB. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
